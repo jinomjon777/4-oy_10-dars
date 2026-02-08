274e59885db721d0804b7a0632d74d7dbed60553
jinomjon777 <jinomjon777@gmail.com>
2026-02-08 11:36:48 +0500

    10-lesson home work

--- router/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const { register } = require("../controller/auth.controller");
-
-const authRouter = Router();
-authRouter.post("/register", register);
-
-module.exports = authRouter;
